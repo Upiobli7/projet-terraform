50c6104f4604a196f7c9dd43c4cd5477da5549f2
Upiobli7 <piobliurbain@gmail.com>
2024-12-01 20:46:01 +0100

    add file

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,15 +6,8 @@ module "ressource_group" {
 
 module "app_service" {
   source = "./modules/app_service"
-  location = module.ressource_group.location
-  resource_group_name = module.ressource_group.ressource_group_name
   tier = "Basic"
   size = "B1"
 }
 
-# modules/app_service/outputs.tf
-output "app_service_url" {
-  description = "URL de l'App Service"
-  value       = module.app_service.default_site_hostname
-}
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,2 @@
-# Définitions des sorties Terraform
-# Sortie de l'URL de l'App Service
-output "app_service" {
-  description = "URL de l'App Service"
-  value       = module.app_service.default_site_hostname
-}
+
 
